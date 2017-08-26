88c78377170e531e3a69272e614176f68fb35631
xudafeng <xudafeng@126.com>
2017-08-26 17:54:56 +0800

    add mock

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 'use strict';
 
+const fs = require('fs');
 const koa = require('koa');
 const ipv4 = require('ipv4');
+const path = require('path');
 
 const app = koa();
 
-const macacaReporter = require('macaca-reporter');
+const macacaReporterRender = require('macaca-reporter/lib/render');
 
 app.use(function * () {
-  console.log(macacaReporter);
-  this.body = 'Hi, it works';
+  const mockData = require('macaca-reporter/test/mock');
+  console.log(JSON.stringify(mockData));
+  macacaReporterRender(mockData, {});
+  this.body = fs.readFileSync(path.join(__dirname, 'reports', 'index.html'), 'utf8');
 });
 
 const port = 9001;
