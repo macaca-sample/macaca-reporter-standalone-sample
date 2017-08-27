ff6abf887e574d14822e889c5f58fe5c639c4db3
xudafeng <xudafeng@126.com>
2017-08-27 17:28:03 +0800

    update

--- index.js
+++ index.js
@@ -4,16 +4,55 @@ const fs = require('fs');
 const koa = require('koa');
 const ipv4 = require('ipv4');
 const path = require('path');
+const detectPort = require('detect-port');
+var server = require('http').createServer();
+var io = require('socket.io')(server);
 
 const app = koa();
 
 const macacaReporterRender = require('macaca-reporter/lib/render');
 
+var socketed = false;
+
 app.use(function * () {
+
   const mockData = require('macaca-reporter/test/mock');
+
   console.log(JSON.stringify(mockData));
-  macacaReporterRender(mockData, {});
+
+  var port = yield detectPort(56788);
+
+  macacaReporterRender(mockData, {
+    socket: {
+      server: `http://localhost:${port}`
+    }
+  });
   this.body = fs.readFileSync(path.join(__dirname, 'reports', 'index.html'), 'utf8');
+
+  if (socketed) {
+    return;
+  }
+
+  socketed = true;
+
+  server.listen(port);
+
+  io.on('connection', function(socket) {
+
+    console.log('socket connection');
+
+    setInterval(() => {
+      console.log('broadcast');
+      mockData.stats.passes++;
+      socket.broadcast.emit('update reporter', mockData);
+    }, 1000);
+
+    socket.on('disconnect', function() {
+
+    });
+
+  });
+
 });
 
 const port = 9001;
